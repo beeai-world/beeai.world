2e1b8c5aa8ac81eb06d72d3297749b6fb3413e37
bahadirciloglu <bahadirciloglu@gmail.com>
2025-05-02 10:57:02 +0300

    Fix: HelmetProvider and BrowserRouter hierarchy to resolve Vercel build issues

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <HelmetProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
+      <BrowserRouter>
+        <HelmetProvider>
+          <Toaster />
+          <Sonner />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/blog" element={<Blog />} />
@@ -27,8 +27,8 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
-      </HelmetProvider>
+        </HelmetProvider>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
